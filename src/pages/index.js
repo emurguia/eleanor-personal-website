fb351e9545bd4dddf139fe6845bd3408fe826d4c
Eleanor Murguia <eleanormurguia@gmail.com>
2019-12-26 15:20:51 -0800

    remove netlfiy starter, add basic starter

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styled from 'styled-components'
+
+const StyledConstruction = styled.div`
+h1 {
+  text-align: center;
+}
+margin-top: 60px;
+margin-bottom: 60px;
+
+`;
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Eleanor Murguia" />
+    <StyledConstruction>
+    <h1>Under Construction</h1>
+    </StyledConstruction>
+  </Layout>
+)
+
+export default IndexPage
