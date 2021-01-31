c69f10c72dcb0990a9c2df51104146a689b78ea8
Eleanor Murguia <emurguia@grubhub.com>
2021-01-31 13:19:29 -0500

    Eleanor 101

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import "./layout.css"
+import styled from 'styled-components'
 
+
+const Footer = styled.footer `
+    margin-top: 200px;
+`;
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -25,7 +21,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
@@ -35,11 +30,11 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
+        <Footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </Footer>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,37 @@ h1 {
 }
 margin-top: 60px;
 margin-bottom: 60px;
+`;
 
+const Contact = styled.div`
+  p { 
+    text-align: center;
+  }
 `;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Eleanor Murguia" />
     <StyledConstruction>
-    <h1>Under Construction</h1>
-    </StyledConstruction>
+    <h1>Eleanor Murguia</h1>
+        </StyledConstruction>
+
+    <div> 
+      <p>Eleanor is a Software Engineer currently working on grubhub.com and seamless.com. </p>
+       <p>She is excited by beautiful user interfaces and refactoring legacy code. </p>
+       <p> She loves TypeScript and well documented processes. </p>
+       <p>She graduated from Barnard College in 2019 with a degree in Computer Science and spent several semesters as a TA.</p>
+       <p>She is passionate about sharing the joys of coding and is dedicated to creating inclusive spaces.  </p>
+    </div>
+    <Contact>
+    <p>
+        <a href="https://www.linkedin.com/in/eleanormurguia/"> Connect with her on LinkedIn </a>
+    </p>
+    <p>
+      <a href="/EleanorResume.pdf">View her resume</a>
+    </p>
+
+    </Contact>
   </Layout>
 )
 
